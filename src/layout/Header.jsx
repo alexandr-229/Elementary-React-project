68c8f3dda28c259d7683959da68e530d11fdb06f
Sasha <ivanpolarus2009@gmail.com>
2022-07-31 13:52:24 +0300

    add header and footer

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+    return (
+        <nav className="green darken-1">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">Elementary React-project</a>
+                <ul id="nav-mobile" className="right hide-on-med-down">
+                    <li>
+                        <a href="!#">Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
